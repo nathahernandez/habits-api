dfa2e71115012f25e663ae25e1db96129b5ff708
Nathã Hernandez <nathahcorrea@gmail.com>
2023-01-23 14:43:45 -0300

    feat: routes moved for best folders structure and organization

---
+++ src/router/index.ts
@@ -0,0 +1,13 @@
+import { FastifyInstance } from "fastify"
+import controllers from "../controllers"
+
+export const appRouter = async (app : FastifyInstance) => {
+	try {
+		app.post("/habits", controllers.habits.post)
+		app.get("/day", controllers.day.get)
+	}
+	catch (err) {
+		console.log(`Router error: ${err}`)
+	}
+
+}
